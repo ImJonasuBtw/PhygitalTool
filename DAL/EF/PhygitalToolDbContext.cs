48e2390f3d1fb18dd2fae84e69350575b9331535
Jonas <jonas.nikolopoulos@student.kdg.be>
2024-03-08 21:35:48 +0100

    Sprint 1 : Entity Framework Start - Not working yet

--- DAL/EF/PhygitalToolDbContext.cs
+++ DAL/EF/PhygitalToolDbContext.cs
@@ -1,8 +1,41 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics;
+using Domain.Flow;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace DAL.EF;
 
 public class PhygitalToolDbContext : DbContext
 {
-    //test
+    private DbSet<Question> Questions { get; set; }
+    
+    public PhygitalToolDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("");
+
+        }
+
+        optionsBuilder.LogTo(message => Debug.WriteLine(message), LogLevel.Information);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+    
+    public bool CreateDataBase(bool dropDatabase)
+    {
+        if (dropDatabase)
+        {
+            Database.EnsureDeleted();
+        }
+        return Database.EnsureCreated();
+    }
 }
