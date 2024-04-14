148e2dd481d02968b0b44947a1bdea2b89317f1d
Amarylis Van den Broeck <amarylis.vandenbroeck@student.kdg.be>
2024-04-14 11:34:51 +0000

    Revert "Merge branch 'temoraray-hannelie' into 'Development'"
    This reverts merge request !46

--- DAL/EF/DataSeeder.cs
+++ DAL/EF/DataSeeder.cs
@@ -73,7 +73,7 @@ public static class DataSeeder
         //  Single choice
         var singleChoice1 = new Question(1,
             "Als jij de begroting van je stad of gemeente zou opmaken, waar zou je dan in de komende jaren vooral op inzetten?",
-            QuestionType.SingleChoice, true);
+            QuestionType.SingleChoice);
         //  Multiple choice
         var multipleChoice1 = new Question(2,
             "Wat zou jou helpen om een keuze te maken tussen de verschillende partijen?",
@@ -105,9 +105,9 @@ public static class DataSeeder
 
         // Creating Answer Possibilities
         // Linear Flow
-        var answerPossibility1 = new AnswerPossibility(1, "natuur & ecologiee", 4);
+        var answerPossibility1 = new AnswerPossibility(1, "natuur & ecologie");
         var answerPossibility2 = new AnswerPossibility(2, "vrije tijd, sport, cultuur");
-        var answerPossibility3 = new AnswerPossibility(3, "onderwijs & kinderopvang", 3);
+        var answerPossibility3 = new AnswerPossibility(3, "onderwijs & kinderopvang");
         var answerPossibility4 = new AnswerPossibility(4, "huisvesting");
         var answerPossibility5 = new AnswerPossibility(5, "gezondheidszorg & welzijn");
         var answerPossibility6 = new AnswerPossibility(6, "Ondersteunen van lokale handel");
